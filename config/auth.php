e5a972b973c2e527c43abd5663a2a6b3c8ab3e75
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-07-12 22:35:01 -0300

    middleware de admin

--- config/auth.php
+++ config/auth.php
@@ -64,7 +64,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', Usuario::class),
+            'model' => Usuario::class,
         ],
 
         // 'users' => [
